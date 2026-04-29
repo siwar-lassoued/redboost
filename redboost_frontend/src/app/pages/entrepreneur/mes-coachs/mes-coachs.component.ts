a89b9043fb60ca971dfcdf51cc2e2e879bf069b7
Daouthi-Wassim <daouthi7@gmail.com>
2026-04-30 00:37:50 +0100

    ..

--- redboost_frontend/src/app/pages/entrepreneur/mes-coachs/mes-coachs.component.ts
+++ redboost_frontend/src/app/pages/entrepreneur/mes-coachs/mes-coachs.component.ts
@@ -211,7 +211,7 @@ export class MesCoachsComponent implements OnInit {
     const user = this.authSvc.currentUser$.value;
     const userId = user?.id;
     if (userId) {
-      this.matchSvc.getEntrepreneurCoaches(Number(userId)).subscribe(data => {
+      this.matchSvc.getEntrepreneurCoaches(userId).subscribe(data => {
         this.matchings.set(data);
         data.forEach(m => {
           this.loadSlots(m.id, Number(userId));
